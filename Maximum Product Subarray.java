97a8dadc46796e6d7fd12487237258e29d2c4513
Vikash Kumar <88263645+Vikash620404@users.noreply.github.com>
2022-07-19 00:17:40 +0530

    Maximum Product Subarray in java

---
+++ Maximum Product Subarray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProduct(int[] arr) {
+        int n=arr.length;
+         int product=1;
+      int maxProduct=Integer.MIN_VALUE ;
+      for(int i=0;i<n;i++){
+          product*=arr[i];
+          maxProduct=Math.max(maxProduct,product);
+          if(product==0)
+          product=1;
+      }
+      product=1;
+      for(int i=n-1;i>=0;i--){
+          product*=arr[i];
+          maxProduct=Math.max(maxProduct,product);
+          if(product==0)
+          product=1;
+      }
+      return maxProduct;
+    }
+}
