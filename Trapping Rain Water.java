89997b6d711fe5a9d61c16d7d5b222431d94215d
Vikash Kumar <88263645+Vikash620404@users.noreply.github.com>
2022-07-21 23:47:01 +0530

    Trapping Rain Water inn java

---
+++ Trapping Rain Water.java
@@ -0,0 +1,26 @@
+class Solution{
+    
+   
+    static long trappingWater(int arr[], int n) { 
+       int[] left=new int[n];
+       int[] right=new int[n];
+       left[0]=arr[0];
+       for(int i=1;i<n;i++){
+           left[i]=Math.max(arr[i],left[i-1]);
+       }
+       right[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+           right[i]=Math.max(arr[i],right[i+1]);
+       }
+       long ans=0;
+       for(int i=0;i<n;i++){
+           ans+=(Math.min(left[i],right[i])-arr[i]);
+       }
+       return ans;
+    } 
+}
+
+
+//O(N)
+//O(N)
+
